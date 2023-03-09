df8c72224c86fbabaa9318f1f75bf2c6a688ec06
woodyho13@gmail.com <HOphy!19>
2023-03-09 17:49:54 -0600

    TagBot Update

--- src/utils/IC.jl
+++ src/utils/IC.jl
@@ -56,31 +56,20 @@ function SetUpProblemIC!(prob;  ρ = [],
       mul!(sol₀ , grid.rfftplan, vars.ρ);
     end
   end
-  if prob.dye.dyeflag
-    if ρ ==[]
-      warning("User declare the dye but no dye is set")
-    else
-      copyto!(prob.dye.ρ, ρ);
-      mul!(prob.dye.tmp.sol₀, grid.rfftplan, prob.dye.ρ);
-    end
-  end
 
   # Copy the data to both output and solution array
-  if (! prob.flag.e) 
-    for (uᵢ,prob_uᵢ,uᵢind) in zip([ux,uy,uz],[vars.ux,vars.uy,vars.uz],
+  for (uᵢ,prob_uᵢ,uᵢind) in zip([ux,uy,uz],[vars.ux,vars.uy,vars.uz],
                                 [params.ux_ind,params.uy_ind,params.uz_ind])
-      if uᵢ != [] 
-        @views sol₀ =  sol[:, :, :, uᵢind];
-        copyto!(prob_uᵢ,uᵢ);
-        if prob.flag.c 
-          mul!(sol₀ , grid.rfftplan, @. vars.ρ*prob_uᵢ);
-        else
-          mul!(sol₀ , grid.rfftplan, prob_uᵢ);
-        end
+    if uᵢ != []
+      @views sol₀ =  sol[:, :, :, uᵢind];
+      copyto!(prob_uᵢ,uᵢ);
+      if prob.flag.c 
+        mul!(sol₀ , grid.rfftplan, @. vars.ρ*prob_uᵢ);
+      else
+        mul!(sol₀ , grid.rfftplan, prob_uᵢ);
       end
     end
   end
-  # copy b-field data
   if prob.flag.b 
     for (bᵢ,prob_bᵢ,bᵢind) in zip([bx,by,bz],[vars.bx,vars.by,vars.bz],
                                   [params.bx_ind,params.by_ind,params.bz_ind])
@@ -91,7 +80,6 @@ function SetUpProblemIC!(prob;  ρ = [],
       end
     end
   end
-  # copy solid domin data
   if prob.flag.vp
     for (Uᵢ,prob_Uᵢ) in zip([U₀x,U₀y,U₀z],
                             [params.U₀x,params.U₀y,params.U₀z])
@@ -105,6 +93,7 @@ function SetUpProblemIC!(prob;  ρ = [],
       end
     end
   end
+
   return nothing;
   
 end
@@ -178,60 +167,7 @@ function DivFreeSpectraMap( grid;
   return Fx,Fy,Fz;
   
 end
-function DivFreeSpectraMap2( Nx::Int, Ny::Int, Nz::Int;
-                            Lx = 2π,
-                            dev = CPU(), 
-                            k0 = 1.0, σ² = 1.0,  b = 1, T = Float64, k_peak = 0.0)
-  grid = ThreeDGrid(dev; nx = Nx, Lx = Lx, ny=Ny, nz=Nz, T = T,nthreads = 8);
-  return DivFreeSpectraMap( grid; k_peak = k_peak, P = P, k0 = k0, b = b);
-end
 
-function DivFreeSpectraMap2( grid;
-                             k_peak = 0.0, P = 1, k0 = 1.0, σ² = 1.0,  b = 1)
-    
-  T = eltype(grid);  
-  @devzeros typeof(grid.device) Complex{T} (grid.nkr,grid.nl,grid.nm) eⁱᶿ Fk Fxh Fyh Fzh 
-  @devzeros typeof(grid.device)         T  (grid.nx ,grid.ny,grid.nz) Fx Fy Fz
-  
-  kx,ky,kz  = grid.kr,grid.l,grid.m;  
-  Lx,Ly,Lz  = grid.Lx,grid.Ly,grid.Lz;
-  dx,dy,dz  = grid.dx,grid.dy,grid.dz;
-  k⁻¹  = @. √(grid.invKrsq);
-  k    = @. √(grid.Krsq);
-  k⊥   = @. √(kx^2 + ky^2);
-  dk⁻² = @. 1/(k+1)^2;
-  CUDA.@allowscalar Fk[1,1,1] = 0.0;
-  @. Fk[1,:,:] .= 0;
-  Fk  = @. √(exp(-(k.-k0)^2/σ²)/2/π)*k⁻¹;
-    
-  e1x = @.  ky/k⊥;
-  e1y = @. -kx/k⊥;
-  e2x = @. kx*kz/k⊥*k⁻¹;
-  e2y = @. ky*kz/k⊥*k⁻¹;
-  e2z = @. -k⊥*k⁻¹;
-  e1x[isnan.(e1x)] .= 0;
-  e1y[isnan.(e1y)] .= 0;
-  e2y[isnan.(e2y)] .= 0;
-  e2x[isnan.(e2x)] .= 0;
-    
-  # Work out the random conponement 
-  randN = grid.device == CPU() ? rand : CUDA.rand;
-  eⁱᶿ .= exp.(im.*randN(T,grid.nkr,grid.nl,grid.nm)*2π);
-  @. Fxh += Fk*eⁱᶿ*e2x;
-  @. Fyh += Fk*eⁱᶿ*e2y;
-  @. Fzh += Fk*eⁱᶿ*e2z;
-  
-  dealias!(Fxh, grid)
-  dealias!(Fyh, grid)
-  dealias!(Fzh, grid)
-
-  ldiv!(Fx, grid.rfftplan, deepcopy(Fxh));  
-  ldiv!(Fy, grid.rfftplan, deepcopy(Fyh));
-  ldiv!(Fz, grid.rfftplan, deepcopy(Fzh));
-
-  return Fx,Fy,Fz;
-  
-end
 
 """
     readMHDFlows(FileName)
